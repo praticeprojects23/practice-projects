0d381516dcaf8a49253a380b19ccef730a683762
Manoj Sharma <manojsharma.resume@gmail.com>
2026-03-26 16:39:47 -0400

    cleanup on files not needed to track

--- selenium/simpleScripts/selenium-practice/src/main/java/com/practice/framework/config/ConfigReader.java
+++ selenium/simpleScripts/selenium-practice/src/main/java/com/practice/framework/config/ConfigReader.java
@@ -5,10 +5,12 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
-
+	//This will hold key-value pairs from the config file.
     private static final Properties properties = new Properties();
-
+    //This block runs automatically the first time the class is loaded.
+    //So when ConfigReader is first used, Java immediately tries to load the properties file.
     static {
+    	//This looks for the file in the classpath
         InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties");
 
         if (input == null) {
